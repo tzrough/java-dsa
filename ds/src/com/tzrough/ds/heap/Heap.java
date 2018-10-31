abef102a960098f436a857caba398675b79cd22b
zhf <732898328@qq.com>
2018-10-31 13:56:31 +0800

    add comment

--- ds/src/com/tzrough/ds/heap/Heap.java
+++ ds/src/com/tzrough/ds/heap/Heap.java
@@ -12,6 +12,7 @@ public abstract class Heap<T extends Comparable<T>>
 		build(heapSize);
 	}
 	
+	/** 获取根节点下标 **/
 	public int parent(int i) 
 	{
 		if(i == 0)
@@ -20,29 +21,30 @@ public abstract class Heap<T extends Comparable<T>>
 			return (i - 1) / 2;
 	}
 
-	
+	/** 获取左孩子节点下标 **/
 	public int leftChild(int i) 
 	{
 		return (i + 1) * 2 - 1;
 	}
 
-	
+	/** 获取右孩子节点下标 **/
 	public int rightChild(int i) 
 	{
 		return (i + 1) * 2;
 	}
 	
-	
 	/**
-	 * @param data 	存入堆的数据
-	 * @param    i  堆中需要调整位置的元素下标
+	 * 调整堆中某个节点的位置
+	 * @param data 		存入堆的数据
+	 * @param heapsize  此变量用于重构部分堆节点
+	 * @param i  		堆中需要调整位置的元素下标
 	 */
 	public abstract void heapify(T[] data, int heapsize, int i);
 	
+	/** 重建堆结构 **/
 	public void build(int heapSize) 
 	{
 		int lastParentIndex = parent(heapSize - 1);
-		
 		// 从最后一个有孩子的父节点开始
 		// 从右到左， 从下到上， 逐个调整位置
 		for(int i = lastParentIndex; i >= 0; i--)
@@ -51,6 +53,13 @@ public abstract class Heap<T extends Comparable<T>>
 		}
 	}
 	
+	/**
+	 * 排序思路
+	 * 1> 将大根(小根)堆根节点置换到堆尾
+	 * 2> 将大根(小根)堆大小缩小1，去除最后一个节点
+	 * 3> 将缩小后的大根(小根)堆重新构建
+	 * 4> 重复1-3步骤，直至堆只剩下两个节点
+	 */
 	public T[] sort() 
 	{
 		int lastIndex = heapSize - 1;
@@ -67,7 +76,9 @@ public abstract class Heap<T extends Comparable<T>>
 		return data;
 	}
 	
-	
+	/**
+	 * 交换堆的两个节点
+	 */
 	public void swap(int index1, int index2) 
 	{
 		T temp = data[index1];
@@ -76,7 +87,9 @@ public abstract class Heap<T extends Comparable<T>>
 		data[index2] = temp;
 	}
 	
-	
+	/**
+	 * 向堆中添加新的节点 
+	 */
 	@SuppressWarnings("unchecked")
 	public void add(T t) 
 	{
@@ -92,7 +105,9 @@ public abstract class Heap<T extends Comparable<T>>
 		build(heapSize);
 	}
 
-	
+	/**
+	 * 打印堆中的节点
+	 */
 	public void print() 
 	{
 		for (int i = 0; i < data.length; i++)
--- ds/src/com/tzrough/ds/heap/MaxHeapSort.java
+++
@@ -1,9 +0,0 @@
-package com.tzrough.ds.heap;
-
-public class MaxHeapSort
-{
-
-	
-	
-	
-}
